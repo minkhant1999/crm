c6c319dcdfb4e5b4127b332f160ec102636d3e3a
minkhant1999 <minkhant16999@gmail.com>
2020-06-12 00:31:03 +0630

    deals

--- view/deals.php
+++ view/deals.php
@@ -1,2 +1,20 @@
-<?php
-include "header.php";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deals</title>
+    <link rel="stylesheet" type="text/css" href="https://etherio.github.io/cdn/semantic/semantic.css" />
+    <script src="https://etherio.github.io/cdn/jquery/jquery.min.js"></script>
+    <script src="https://etherio.github.io/cdn/semantic/semantic.js" defer></script>
+</head>
+
+<body>
+    <?php
+    include "menu.php";
+    ?>
+
+</body>
+
+</html>
--- view/menu.php
+++ view/menu.php
@@ -3,21 +3,16 @@
         <img class="logo" src="assets/images/logo.png">
         CRM
     </a>
-    <a href="#" class="item">Deals</a>
-    <a class="ui dropdown item">
+    <a href="deals.php" class="item">Deals</a>
+    <div class="ui simple dropdown item">
         Contacts
         <i class="dropdown icon"></i>
         <div class="menu">
-            <div class="item">Action</div>
-            <div class="item">Another Action</div>
-            <div class="item">Something else here</div>
-            <div class="divider"></div>
-            <div class="item">Separated Link</div>
-            <div class="divider"></div>
-            <div class="item">One more separated link</div>
+            <a class="item" href="persons.php">People</a>
+            <a class="item" href="organizations.php">Organizations</a>
         </div>
-    </a>
-    <a href="#" class="item">Products</a>
+    </div>
+    <a href="products.php" class="item">Products</a>
     <div class="right menu">
         <div class="item">
             <div class="ui transparent inverted icon input">
